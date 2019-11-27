00cf35ffcbd21c7ac8414724861539834eded070
Korisnik <Korisnik@DESKTOP-2CRP2O3>
2019-11-27 10:11:25 +0100

    Add project files.

---
+++ Classes/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Liste
+            Liste();
+            Console.ReadKey();
+        }
+
+        static void Liste()
+        {
+            List<int> numbers = new List<int>();
+
+            numbers.Add(10);
+            numbers.Add(25);
+            numbers.Add(39);
+            numbers.Add(2);
+            numbers.Add(7);
+
+            foreach (int num in numbers)
+            {
+                Console.WriteLine(num);
+            }
+
+            numbers.Sort();
+
+            Console.WriteLine("After calling Sort() method");
+            foreach (int num in numbers)
+            {
+                Console.WriteLine(num);
+            }
+
+        }
+    }
+}
