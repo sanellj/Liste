8f24af838a2c40799d007e9820c0107e357e65a3
Korisnik <Korisnik@DESKTOP-2CRP2O3>
2019-11-27 12:17:42 +0100

    Dodali smo klasu - log

---
+++ Classes/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Classes
+{
+    class Class1
+    {
+        public static string LogFileName = "C:\\Vjezba.log";
+
+        public static void Log(string LogMessage)
+        {
+            using (StreamWriter file = new StreamWriter(LogFileName, true))
+            {
+                file.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss K"));
+                file.WriteLine(LogMessage);
+                file.WriteLine("********");
+                file.WriteLine();
+
+            }
+        }
+    }
+}
